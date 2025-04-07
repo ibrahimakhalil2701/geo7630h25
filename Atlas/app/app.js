a9974fc8dbb947e94c27215199c719a445aa44fc
ibrahimakhalil2701 <ea990824@ens.uqam.ca>
2025-04-07 19:48:17 +0000

    Copier le dossier Atlas

---
+++ Atlas/app/app.js
@@ -0,0 +1,72 @@
+// création de la carte Mapbox GL
+var map = new maplibregl.Map({
+    container: 'map', // identifiant de l'élément HTML conteneur de la carte
+    style: 'https://api.maptiler.com/maps/dataviz/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
+    center: [-73.55, 45.55], // position centrale de la carte
+    zoom: 9, // niveau de zoom initial
+    hash: true // activation du hash pour la gestion de l'historique de la carte
+});
+
+////////////////// Lab 11 ////////////////////
+// map.addSource('arrondissementsSource', arrondissementsSource); // Ajout de la source
+// map.addLayer(arrondissementsLayer); // Ajout de la couche des arrondissements
+// map.addLayer(arrondissementsLabelsLayer);
+//////////////////////////////////////////////
+
+function loadTeam(teamName) {
+    // Vider toutes les divs
+    document.getElementById('Equipe1').innerHTML = '';
+    document.getElementById('Equipe2').innerHTML = '';
+    document.getElementById('Equipe3').innerHTML = '';
+    document.getElementById('Equipe4').innerHTML = '';
+    document.getElementById('Equipe5').innerHTML = '';
+    // Charger le contenu de l'équipe sélectionnée
+    fetch('./' + teamName + '/index.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(teamName).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with fetching the team content:', error);
+        });
+}
+
+
+
+const myLayers = ['grid', 'buffer', 'rdp', 'union', 'joined']
+
+// Cette fonction est appelée lorsque la carte est chargée.
+map.on('load', function () {
+    // Charge une couche de points aléatoires.
+    loadRandomPointsLayer()
+    // Ajoute un contrôle de légende personnalisé.
+    // Lab 12
+    // map.addControl(new MaplibreLegendControl({ 
+    //     rdp: "rdp", 
+    //     grid: 'grid',
+    //     union: 'union',
+    //     buffer: 'buffer',
+    //     joined: 'joined'
+    // }, { onlyRendered: true }), "bottom-left");
+});
+
+// Cette fonction est appelée lorsque la carte est en attente de fin de motion
+map.on('idle', function () {
+    // Récupère les couches de style de la carte.
+    const layers = map.getStyle().layers;
+    // Parcourt toutes les couches.
+    layers.forEach((layer) => {
+        // Si la couche est 'joined', met à jour le compteur dynamique.
+        if (layer.id == 'joined') {
+            dynamicCount()
+        // Sinon, si la couche est 'rdp', met à jour le compteur de fonctionnalités.
+        } else if (layer.id == 'rdp') {
+            featureCount()
+        }        
+    });
+});
